c291c68f0d13ca330dc188893f9ce512119706c9
RichardRahm <richieboy928@gmail.com>
2021-02-02 18:33:09 -0700

    Created Prototype 2 Animal Stampede & Created Player Movement

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float hInput;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //allows the player to use the arrow keys to move left and right
+        hInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+       
+       //constrains player on left side
+        if(transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+        }
+}

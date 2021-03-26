4f4d25f1e5fd80ca5c1a2a228c9b5692dc892e32
RichardRahm <richieboy928@gmail.com>
2021-03-26 13:37:19 -0600

    Basic Controls Added to Final Project

---
+++ Final Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+
+{
+   
+    public bool isSlowEnough = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When player left clicks, the money will show on the left as a "projectile"
+        if (Input.GetKeyDown(KeyCode.Mouse0) && isSlowEnough)
+            {
+                Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            }
+    }
+
+
+}
